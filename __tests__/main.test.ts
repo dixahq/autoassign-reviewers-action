64e57eecfed9ded6c4c4e313d684c4037357b479
Jakob Hyldtoft <jht@dixa.com>
2024-02-15 11:07:15 +0100

    feat: upgrade dependencies and node runtime
    chore: add prettierrc

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,54 +1,68 @@
-const nock = require('nock')
-const path = require('path')
+const path = require('path');
+import { http, HttpResponse } from 'msw';
+import { setupServer } from 'msw/node';
+
+const worker = setupServer(
+  http.get(
+    'https://api.github.com/repos/foo/bar/pulls/10',
+    ({ request, params, cookies }) => {
+      return HttpResponse.json(
+        {},
+        {
+          status: 200,
+        },
+      );
+    },
+  ),
+  http.post(
+    'https://api.github.com/repos/foo/bar/pulls/10/requested_reviewers',
+    ({ request, params, cookies }) => {
+      return HttpResponse.json(
+        {},
+        {
+          status: 200,
+        },
+      );
+    },
+  ),
+);
+
+beforeAll(() => {
+  worker.listen();
+});
+
+afterAll(() => {
+  worker.close();
+});
 
 describe('Team', () => {
   it('It requests a review to a team', async () => {
-    const teams = 'hello,team'
-    const repoToken = 'token'
-    process.env['INPUT_TEAMS'] = teams
-    process.env['INPUT_REPO-TOKEN'] = repoToken
+    const teams = 'hello,team';
+    const repoToken = 'token';
+    process.env['INPUT_TEAMS'] = teams;
+    process.env['INPUT_REPO-TOKEN'] = repoToken;
 
-    process.env['GITHUB_REPOSITORY'] = 'foo/bar'
-    process.env['GITHUB_EVENT_PATH'] = path.join(__dirname, 'payload.json')
+    process.env['GITHUB_REPOSITORY'] = 'foo/bar';
+    process.env['GITHUB_EVENT_PATH'] = path.join(__dirname, 'payload.json');
 
-    nock('https://api.github.com')
-    .persist()
-    .get('/repos/foo/bar/pulls/10')
-    .reply(200)
+    const main = require('../src/main');
 
-    nock('https://api.github.com')
-      .persist()
-      .post('/repos/foo/bar/pulls/10/requested_reviewers')
-      .reply(200)
-      
-    const main = require('../src/main')
-
-    await main.run()
-  })
-})
+    await main.run();
+  });
+});
 
 describe('Reviewer', () => {
   it('It requests a review to a person', async () => {
-    const reviewer = 'person'
-    const repoToken = 'token'
-    process.env['INPUT_PERSONS'] = reviewer
-    process.env['INPUT_REPO-TOKEN'] = repoToken
+    const reviewer = 'person';
+    const repoToken = 'token';
+    process.env['INPUT_PERSONS'] = reviewer;
+    process.env['INPUT_REPO-TOKEN'] = repoToken;
 
-    process.env['GITHUB_REPOSITORY'] = 'foo/bar'
-    process.env['GITHUB_EVENT_PATH'] = path.join(__dirname, 'payload.json')
+    process.env['GITHUB_REPOSITORY'] = 'foo/bar';
+    process.env['GITHUB_EVENT_PATH'] = path.join(__dirname, 'payload.json');
 
-    nock('https://api.github.com')
-    .persist()
-    .get('/repos/foo/bar/pulls/10')
-    .reply(200)
+    const main = require('../src/main');
 
-    nock('https://api.github.com')
-      .persist()
-      .post('/repos/foo/bar/pulls/10/requested_reviewers')
-      .reply(200)
-
-    const main = require('../src/main')
-
-    await main.run()
-  })
-})
+    await main.run();
+  });
+});
